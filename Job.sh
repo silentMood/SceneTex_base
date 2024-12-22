e5a1885b17119b8692b70a819cec35d548620a5a
Herbert <herbertdaicoding@gmail.com>
2024-12-22 00:41:00 -0500

    fixed

--- Job.sh
+++ Job.sh
@@ -4,15 +4,12 @@
 #SBATCH --mail-type=ALL        ## Receive all email type notifications 
 #SBATCH --chdir=./             ## Use currect directory as working directory 
 #SBATCH --nodes=1              ## Number of nodes to run on 
-#SBATCH --ntasks-per-node=32   ## Number of cores 
-#SBATCH --cpus-per-task=1      ## Number of MPI threads 
+#SBATCH --ntasks=1   ## Number of cores 
+#SBATCH --cpus-per-task=8      ## Number of MPI threads 
 #SBATCH --mem=160G             ## Assign 160G memory per node
 #SBATCH --gpus=1
 #SBATCH -p pt
 
-conda init tcsh
-source ~/.tcshrc
-conda activate scenetex
 ############################ dependencies install ############################
 # install PyTorch 2.0.1
 conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia -y
@@ -22,7 +19,7 @@ conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
 conda install -c bottler nvidiacub -y
 
 # install PyTorch3D
-conda install pytorch3d -c pytorch3d -y
+conda install pytorch3d pytorch-lightning -c pytorch3d -c conda-forge -y
 
 conda install xformers -c xformers -y
 
--- configure.sh
+++ configure.sh
@@ -4,6 +4,10 @@ module load python/3.9.1/default
 module load cuda/11.8/default
 ############################ module install end ############################
 
+conda init tcsh
+source ~/.tcshrc
+conda activate scenetex
+
 # create and activate the conda environment
 conda create -n scenetex python=3.9
 conda activate scenetex
