2fcc8859bc29f3bc7327ac2894cd9ced2d38a04b
Herbert <herbertdaicoding@gmail.com>
2025-01-23 20:04:46 -0500

    bugfix

--- install.sh
+++ install.sh
@@ -22,19 +22,36 @@ setenv CONDA_PKGS_DIRS /speed-scratch/$USER/sceneTex/pkgs
 setenv CMAKE_CUDA_COMPILER /encs/pkg/cuda-11.5/root/bin/nvcc
 setenv CMAKE_C_COMP
 
-git clone --recursive https://github.com/NVlabs/tiny-cuda-nn.git
-cd tiny-cuda-nn
-cmake -DCMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc -DCMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/g++ . -B build
-cmake --build build --config RelWithDebInfo -j
-cd bindings/torch
-python setup.py install
+conda create -n scenetex python=3.9
+conda activate scenetex
 
-date
-cd /encs/pkg/cuda-11.5/root/
-date
+conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
 
-setenv CMAKE_CXX_COMPILER /encs/pkg/gcc-9.3.0/root/bin/gcc
+# install runtime dependencies for PyTorch3D
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath
+conda install -c bottler nvidiacub
 
-cmake -DCMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc -DCMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/g++ . -B build
+# install PyTorch3D
+conda install pytorch3d -c pytorch3d
 
-cmake . -B CMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc CMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc build
+conda install xformers -c xformers
+
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+
+pip install -r requirements.txt
+# git clone --recursive https://github.com/NVlabs/tiny-cuda-nn.git
+# cd tiny-cuda-nn
+# cmake -DCMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc -DCMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/g++ . -B build
+# cmake --build build --config RelWithDebInfo -j
+# cd bindings/torch
+# python setup.py install
+
+# date
+# cd /encs/pkg/cuda-11.5/root/
+# date
+
+# setenv CMAKE_CXX_COMPILER /encs/pkg/gcc-9.3.0/root/bin/gcc
+
+# cmake -DCMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc -DCMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/g++ . -B build
+
+# cmake . -B CMAKE_CXX_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc CMAKE_C_COMPILER=/encs/pkg/gcc-9.3.0/root/bin/gcc build
